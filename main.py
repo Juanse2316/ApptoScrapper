93ee22dab5c1b144591142c08b7053e6957bcf09
Juanse2316 <ladinomendietajuanse@gmail.com>
2024-01-10 12:08:32 -0500

    adding new csv fields and user interface

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
 import flet as ft
+from scrapper import MercadoLibreScraper
+from datetime import datetime
+
 
 def main(page: ft.Page):
-    container = ft.Container()
-    page.add(container)
+    def search_products(e):
+        query = txt_search.value
+        scraper = MercadoLibreScraper("https://listado.mercadolibre.com.co/")
+        products = scraper.search(query)
+        scraper.save_to_csv(products, f"mercadolibre_products_{query}.csv")
+        txt_result.value = f"Resultados guardados en: mercadolibre_products_{query}_{datetime.now().strftime('%Y%m%d_%H%M%S')}.csv"
+
+    # Creando los componentes de la UI
+    txt_search = ft.TextField(hint_text="Escribe aquí el producto a buscar", expand=1)
+    btn_search = ft.ElevatedButton(text="Buscar", on_click=search_products)
+    txt_result = ft.TextField(label="Resultado", multiline=True, expand=1,)
+
+    # Añadiendo los componentes a la página
+    page.add(txt_search, btn_search, txt_result)
 
 ft.app(target=main)
--- scrapper.py
+++ scrapper.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup 
 import pandas as pd
-from datetime import datetime
+
 
 
 url = "https://listado.mercadolibre.com.co/"
@@ -14,33 +14,48 @@ class MercadoLibreScraper:
         url = f"{self.base_url}{query.replace(' ', '-')}"
         products = []
 
-        while url:
-            response = requests.get(url)
-            soup = BeautifulSoup(response.text, 'html.parser')
-            products.extend(self._parse_results(soup))
-            url = self._get_next_page(soup)
+        try:
+            while url:
+                response = requests.get(url)
+                soup = BeautifulSoup(response.text, 'html.parser')
+                products.extend(self._parse_results(soup))
+                url = self._get_next_page(soup)
+        except Exception as e:
+            print(f"Error durante la búsqueda: {e}")
 
         return products
-
     def _parse_results(self, soup: BeautifulSoup) -> list:
         products = []
         for item in soup.find_all("li", class_="ui-search-layout__item"):
             title = item.find("h2", class_="ui-search-item__title")
             price = item.find("span", class_="andes-money-amount__fraction")
+            rating = item.find("span", class_="ui-search-reviews__rating-number")
+            rating = item.find("span", class_="ui-search-reviews__rating-number")
+            discount_tag = item.find("span", class_="ui-search-price__discount")
+
         
             try:
                 clean_price = price.text.strip().replace(',', '').replace('.', '').replace('$', '')
+                clean_rating = None if rating is None else rating.text.strip()
+                has_discount = discount_tag is not None
+                discount_value = None
+                if has_discount:
+                    discount_value = int(discount_tag.text.strip().replace('%', '').replace(' ', '').replace('OFF', ''))
+
                 products.append({
-                    "title": title.text.strip(),
-                    "price": int(clean_price)
+                    "Title": title.text.strip(),
+                    "Price": int(clean_price),
+                    "Rating": clean_rating,
+                    "Has_discount": has_discount,  
+                    "Discount": discount_value,  
                     })
             except ValueError:
-                # Manejo de situaciones donde el precio no se puede convertir
+                
                 print(f"Error al convertir el precio: {price.text}")
         return products
 
     def _get_next_page(self, soup):
-        next_page = soup.find("a", class_="andes-pagination__link", attrs={"title": "Siguiente"})
+        next_page = soup.find("a", class_="andes-pagination__link ui-search-link", attrs={"title": "Siguiente"})
         if next_page:
             return next_page.get('href')
         return None
@@ -50,6 +65,6 @@ class MercadoLibreScraper:
         df.to_csv(filename, index=False)
         print(f"Data saved to '{filename}'")
 
-scraper = MercadoLibreScraper(url)
-products = scraper.search("ram ddr3 8gb")
-scraper.save_to_csv(products, f"mercadolibre_products_{datetime.now().strftime('%Y%m%d_%H%M%S')}.csv")
+# scraper = MercadoLibreScraper(url)
+# products = scraper.search("ram ddr3 8gb")
+# scraper.save_to_csv(products, f"mercadolibre_products_{datetime.now().strftime('%Y%m%d_%H%M%S')}.csv")
