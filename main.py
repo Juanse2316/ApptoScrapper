9aff526764bdcbc45b3690104798d93ce685eaaa
Juanse2316 <ladinomendietajuanse@gmail.com>
2024-01-10 19:49:33 -0500

    adding the control logic

--- main.py
+++ main.py
@@ -1,22 +1,24 @@
-import flet as ft
-from Scrapper.scrapper import MercadoLibreScraper
-from datetime import datetime
+import flet 
+from flet import *
 
 
-def main(page: ft.Page):
-    def search_products(e):
-        query = txt_search.value
-        scraper = MercadoLibreScraper("https://listado.mercadolibre.com.co/")
-        products = scraper.search(query)
-        scraper.save_to_csv(products, f"mercadolibre_products_{query}.csv")
-        txt_result.value = f"Resultados guardados en: mercadolibre_products_{query}_{datetime.now().strftime('%Y%m%d_%H%M%S')}.csv"
 
-    # Creando los componentes de la UI
-    txt_search = ft.TextField(hint_text="Escribe aquí el producto a buscar", expand=1)
-    btn_search = ft.ElevatedButton(text="Buscar", on_click=search_products)
-    txt_result = ft.TextField(label="Resultado", multiline=True, expand=1,)
+def main(page: Page):
+    page.bgcolor = "#1A1A40"
+    page.padding = 20
+    page.add(
+        Column(
+            expand= True,
+            controls=[
+                
+            ]
+        )
+    )
+    page.update()
+    pass
 
-    # Añadiendo los componentes a la página
-    page.add(txt_search, btn_search, txt_result)
 
-ft.app(target=main)
+
+
+if __name__ == '__main__':
+    flet.app(target=main)
