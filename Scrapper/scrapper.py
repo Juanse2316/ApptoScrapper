962746f3f7927e9cea844091748d78a197f9a75a
Juanse2316 <ladinomendietajuanse@gmail.com>
2024-01-17 14:16:09 -0500

    Fix Scrapper/scrapper.py

--- Scrapper/scrapper.py
+++ Scrapper/scrapper.py
@@ -12,12 +12,20 @@ class MercadoLibreScraper:
     def __init__(self, base_url):
         self.base_url = base_url
 
+    def _make_request(self, url):
+        try:
+            response = requests.get(url)
+            response.raise_for_status()  
+            return BeautifulSoup(response.text, 'html.parser')
+        except requests.RequestException as e:
+            print(f"Error en la solicitud HTTP: {e}")
+            return None    
+
     def _get_total_pages(self, soup):
         try:
             page_count_text = soup.find("li", class_="andes-pagination__page-count").text
-            
             return int(page_count_text.split()[-1])
-        except Exception as e:
+        except AttributeError  as e:
             print(f"Error extracting total pages: {e}")
             return 1  
 
@@ -27,24 +35,20 @@ class MercadoLibreScraper:
         url = f"{self.base_url}{query.replace(' ', '-')}"
         products = []
         current_page = 1
+
+        soup = self._make_request(url)
+        if not soup:
+            return products
     
-        try:
-            response = requests.get(url)
-            soup = BeautifulSoup(response.text, 'html.parser')
-            total_pages = self._get_total_pages(soup)  
-    
-            while url:
-                update_progress(current_page / total_pages)
-                current_page += 1
-    
-                products.extend(self._parse_results(soup))
-                url = self._get_next_page(soup)
-    
-                if url:
-                    response = requests.get(url)
-                    soup = BeautifulSoup(response.text, 'html.parser')
-        except Exception as e:
-            print(f"Error durante la búsqueda: {e}")
+        total_pages = self._get_total_pages(soup)   
+
+        while url:
+            update_progress(current_page / total_pages)
+            current_page += 1
+
+            products.extend(self._parse_results(soup))
+            url = self._get_next_page(soup)
+            soup = self._make_request(url) if url else None
     
         return products
     
@@ -52,53 +56,37 @@ class MercadoLibreScraper:
     def _parse_results(self, soup: BeautifulSoup) -> list:
         products = []
         for item in soup.find_all("li", class_="ui-search-layout__item"):
-            title = item.find("h2", class_="ui-search-item__title")
-            price = item.find("span", class_="andes-money-amount__fraction")
-            rating = item.find("span", class_="ui-search-reviews__rating-number")
-            discount_tag = item.find("span", class_="ui-search-price__discount")
-            free_shipping_tag = item.find("p", class_="ui-search-item__shipping ui-search-item__shipping--free")
-            variant_element = item.find("span", class_="ui-search-item__group__element ui-search-item__variations-text")
-            variant_count = 0
-            delivery_24h_elemnt = item.find("span", class_="ui-search-item__promise__text ui-search-item__promise__text--last")
-            clean_rating_amount = None
-            if rating:
-                    rating_amount = item.find("span", class_="ui-search-reviews__amount")
-                    clean_rating_amount = int(rating_amount.text.strip().replace('(','').replace(')','')) 
-                    
-            
-
             try:
-                clean_price = price.text.strip().replace(',', '').replace('.', '').replace('$', '')
-                
-                clean_rating = None if rating is None else rating.text.strip()
+                title_element = item.find("h2", class_="ui-search-item__title")
+                price_element = item.find("span", class_="andes-money-amount__fraction")
+                rating_element = item.find("span", class_="ui-search-reviews__rating-number")
+                rating_amount_element = rating_element and item.find("span", class_="ui-search-reviews__amount")
+                discount_tag = item.find("span", class_="ui-search-price__discount")
+                free_shipping_tag = item.find("p", class_="ui-search-item__shipping ui-search-item__shipping--free")
+                variant_element = item.find("span", class_="ui-search-item__group__element ui-search-item__variations-text")
+
+                # Procesamiento de los campos extraídos
+                title = title_element.text.strip() if title_element else None
+                clean_price = int(price_element.text.strip().replace(',', '').replace('.', '').replace('$', '')) if price_element else 0
+                clean_rating = rating_element.text.strip() if rating_element else 0
+                clean_rating_amount = int(rating_amount_element.text.strip().replace('(','').replace(')','')) if rating_amount_element else 0
                 has_discount = discount_tag is not None
-                discount_value = None
+                discount_value = int(discount_tag.text.strip().replace('%', '').replace(' ', '').replace('OFF', '')) if has_discount else None
                 has_free_shipping = free_shipping_tag is not None
                 has_variants = variant_element is not None
-                has_delivery_24h = delivery_24h_elemnt is not None
-                
-                
+                variant_count = int(re.search(r'\d+', variant_element.text).group()) if variant_element else 0
 
-                if has_variants:
-                    match = re.search(r'\d+', variant_element.text)
-                    if match:
-                        variant_count = int(match.group())
-                
-                if has_discount:
-                    discount_value = int(discount_tag.text.strip().replace('%', '').replace(' ', '').replace('OFF', ''))
-                    
                 products.append({
-                    "Title": title.text.strip(),
-                    "Price": int(clean_price),
+                    "Title": title,
+                    "Price": clean_price,
                     "Rating": clean_rating,
                     "Rating Amount": clean_rating_amount,
-                    "Has_discount": has_discount,  
+                    "Has Discount": has_discount,
                     "Discount": discount_value,
-                    "Has free shipping": has_free_shipping,
-                    "Has Variant":has_variants,
-                    "Variant count":variant_count,
-                    "Delivery last 24h": has_delivery_24h,  
-                    })
+                    "Has Free Shipping": has_free_shipping,
+                    "Has Variants": has_variants,
+                    "Variant Count": variant_count,
+                })
             except ValueError:
                 
                 print(f"Error al convertir el precio: {price.text}")
@@ -106,16 +94,14 @@ class MercadoLibreScraper:
 
     def _get_next_page(self, soup):
         next_page = soup.find("a", class_="andes-pagination__link ui-search-link", attrs={"title": "Siguiente"})
-        if next_page:
-            return next_page.get('href')
-        return None
+        
+        return next_page.get('href') if next_page else None
 
     def save_to_csv(self, products, filename, on_success):
         folder_path = "./Scrapper_saved"
-        if not os.path.exists(folder_path):
-            os.makedirs(folder_path)
+        os.makedirs(folder_path, exist_ok=True)  # Crea la carpeta si no existe
 
-        full_path = os.path.join(folder_path, filename)         
+        full_path = os.path.join(folder_path, filename)
 
         df = pd.DataFrame(products)
         
